98874077ab14786abb0cda61bdceedd4c0dfc58e
Ainozu <ainozuuu@gmail.com>
2022-03-15 12:20:10 +0100

    unflip

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -2,20 +2,8 @@ import React from 'react'
 import Card from './common/Card'
 
 class Board extends React.Component {
-  constructor(props) {
-    super();
-      this.state = {
-      isFlipped: true
-    };
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  handleClick(e) {
-    e.preventDefault();
-    this.setState(prevState => ({ isFlipped: !prevState.isFlipped }));
-  }
-
-  render() {
+   render() {
+    var cards=[];
     const array=[1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8,9,9,10,10,11,11,12,12]
     const shuffledArray = array.sort((a, b) => 0.5 - Math.random());
     const hover="hover:border-red-600 "
@@ -25,19 +13,13 @@ class Board extends React.Component {
 
       let items = []
       for (let i = 0; i < 24; i++) {
-        items.push(<Card img={path+shuffledArray[i]+".png"} hover={hover} back={back}/>)
+        items.push(<Card img={path+shuffledArray[i]+".png"} hover={hover} back={back} id={i} cards={cards}/>)
       }
 
     return (
     <div>
-      <div className="grid grid-cols-6 gap-4 p-1" >
-
- 
-      {items}
-      
-      
-
-      
+      <div className="grid grid-cols-6 gap-4 p-1" > 
+        {items}     
       </div>
     </div>
     )
--- src/components/common/card.jsx
+++ src/components/common/card.jsx
@@ -2,24 +2,78 @@ import React from 'react'
 import Cardfront from './Cardfront'
 import Cardback from './Cardback'
 import ReactCardFlip from 'react-card-flip'
+import { click } from '@testing-library/user-event/dist/click';
 
 class Card extends React.Component {
     constructor(props) {
       super();
         this.state = {
-        isFlipped: true
-      };
-      this.handleClick = this.handleClick.bind(this);
+        img:props.img,
+        isFlipped: true,
+        isFounded:false    
+      }
+      
+      this.updateState = this.updateState.bind(this)
+      this.handleClick = this.handleClick.bind(this)
+      
     }
-  
+    updateState(){ 
+      // Changing state 
+      console.log('ápdét')
+      this.setState({isFlipped : true}) 
+    } 
+    
+   
+    unflip(one,two) {
+      one.updateState()
+      two.updateState()
+
+    }
     handleClick(e) {
       e.preventDefault();
       this.setState(prevState => ({ isFlipped: !prevState.isFlipped }));
+      const array=this.props.cards
+      if((array.length != 2)==true)
+      {
+        console.log("nem 2")
+
+        array.push(this)
+      }
+      //console.log(array.length==2)
+      if((array.length == 2)==true)
+        {
+          console.log("2")
+          
+          console.log(this.props.cards)
+          console.log(this.props.cards[0])
+          console.log(this.props.cards[1])
+          var one=this.props.cards[0]
+          var two=this.props.cards[1]
+          //console.log(one)
+          if(one.state['img']===two.state['img'])
+          {
+            console.log("ugyanaz")
+          }
+          else
+          {
+            console.log('nem ugyanaz')
+            setTimeout(() => {  this.unflip(one,two); }, 1000);
+            
+          }
+          this.props.cards.pop()
+          this.props.cards.pop()
+        }
+        
+      
+        //console.log(this.props.cards.length)
+      //console.log(this.props.cards)
     }
+    
   
     render() {
+
       return (
-        <ReactCardFlip isFlipped={this.state.isFlipped} flipDirection="vertical">
+        <ReactCardFlip key={this.props.id} isFlipped={this.state.isFlipped} flipDirection="vertical">
             <Cardfront img={this.props.img} hover={this.props.hover} click={this.handleClick}>
             </Cardfront>
         
