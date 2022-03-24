d1d9ff4fad24d8e721e420dfff5bc4c11bd26720
Ainozu <ainozuuu@gmail.com>
2022-03-25 00:21:15 +0100

    jf

--- src/components/common/Approute.jsx
+++ src/components/common/Approute.jsx
@@ -16,6 +16,7 @@ const Approute = () => {
             <Route exact path="/g1" element={<Board set="set1"/>}/>
             <Route exact path="/g2" element={<Board set="set2"/>}/>
             <Route exact path="/g3" element={<Board set="set3"/>}/>
+            <Route exact path="/g4" element={<Board set="set4"/>}/>
             <Route path="*" element={<Notfound/>}/>
         </Routes>
     </Router>
